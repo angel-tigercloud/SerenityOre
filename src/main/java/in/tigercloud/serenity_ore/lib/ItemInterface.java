a8187b3f9c60cc582f49c566c8f71dab8c7e1a34
Angel-Tigercloud <teridasup@googlemail.com>
2020-11-11 00:29:28 +0100

    Removed junk

--- src/main/java/in/tigercloud/serenity_ore/lib/ItemInterface.java
+++ src/main/java/in/tigercloud/serenity_ore/lib/ItemInterface.java
@@ -1,21 +1,9 @@
 package in.tigercloud.serenity_ore.lib;
 
-import net.minecraft.creativetab.CreativeTabs;
-
-import javax.annotation.Nullable;
-
 /**
  * Interface to build Items
  */
 public interface ItemInterface {
-	/**
-	 * Adds the Basic Data to an item
-	 *
-	 * @param itemName Itemname
-	 * @param creativeTab Creative tab or null for none
-	 */
-	void init(String itemName, @Nullable CreativeTabs creativeTab);
-
 	/**
 	 * Returns the unlocalized Name of the Item
 	 *
