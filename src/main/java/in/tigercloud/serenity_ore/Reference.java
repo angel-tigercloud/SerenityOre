152509a8e113bd8125fe4081d5bb6ad060aeabc3
Angel-Tigercloud <teridasup@googlemail.com>
2020-11-15 12:53:19 +0100

    Changed Version

--- src/main/java/in/tigercloud/serenity_ore/Reference.java
+++ src/main/java/in/tigercloud/serenity_ore/Reference.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.Logger;
 public class Reference {
 	public static final String MOD_ID = "serenity_ore";
 	public static final String MOD_NAME = "Serenity Ore";
-	public static final String VERSION = "@VERSION@";
+	public static final String VERSION = "1.0.0";//"@VERSION@";
 	public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2854,)";
 	public static final String RESOURCE_PREFIX = MOD_ID + ":";
 	public static final String FULL_MAIN_NAMESPACE = "in.tigercloud.serenity_ore";
